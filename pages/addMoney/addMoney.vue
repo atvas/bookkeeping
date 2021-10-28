3a6edf8b64381851d2069ba657b0e95ba685c269
127 <542362045@qq.com>
2021-10-28 18:38:21 +0800

    ss

--- pages/addMoney/addMoney.vue
+++ pages/addMoney/addMoney.vue
@@ -9,8 +9,15 @@
 				<u-form-item label="金额" prop="name">
 					<u-input v-model="form.name" placeholder="请输入金额" input-align="right" :focus="true" />
 				</u-form-item>
-
+				<u-form-item label="日期" prop="time">
+					<u-input v-model="form.time" placeholder="请选择日期" @confirm="confirm" input-align="right" @click="show = true"
+						:focus="true" />
+				</u-form-item>
 			</u-form>
+			<!-- 时间选择 -->
+			<view>
+				<u-picker v-model="show" mode="time"></u-picker>
+			</view>
 		</view>
 	</view>
 </template>
@@ -22,7 +29,9 @@
 				msg: '这是添加页面',
 				form: {
 					name: '',
+					time: ''
 				},
+				show: false,
 				rules: {
 					name: [{
 						required: true,
@@ -46,6 +55,9 @@
 		},
 		onReady() {
 			this.$refs.uForm.setRules(this.rules);
+		},
+		confirm(e){
+			console.log(e)
 		}
 	}
 </script>
